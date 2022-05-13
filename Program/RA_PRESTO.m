a7439aacf1dccdd38c74a88c687bb28fe0535bc2
pwendering <philipp.wendering@gmail.com>
2022-05-13 10:06:17 +0200

    Add documentation and improve plots
    Save similarity plot between subsets and change rho_S to rho_P in
    robustness analysis plot.

--- Program/RA_PRESTO.m
+++ Program/RA_PRESTO.m
@@ -1,13 +1,42 @@
 function [pIds,deltas] = RA_PRESTO(models,expVal,E,epsilon,lambda,theta, n_repeats, set_size)
-% Robustness analysis for PRESTO
+%% [pIds,deltas] = RA_PRESTO(models,expVal,E,epsilon,lambda,theta, n_repeats, set_size)
+% Robustness analysis for PRESTO using randomly-selected subsets of
+% experimental conditions
+% Input:
+%   cell models:            enzyme-constraint metabolic model(s) generated
+%                           using GECKO and with adjusted biomass
+%                           composition dependent on total protein content
+%   double expVal:          scalar or vector containing experimentally
+%                           measured growth rate(s)
+%   double E:               column vector or #proteins x #conditions matrix
+%                           that contains absolute proteomics data for the
+%                           proteins in the model (NaN or zero if unmeasured)
+%   double epsilon:         allowed fold change of a k_cat value
+%   double lambda:          weight for the deviation of
+%                           predicted from experimental growth
+%   double theta:           allowed deviation of the predicted growth rate
+%                           from the given experimental value
+%   double n_repeats:       number of iterations for testing of random
+%                           subsets
+%   double set_size:        size of randomly-selected subsets
+% 
+% Output:
+%   cellstr pIds:           unique protein IDs associated to corrected kcat
+%                           values
+%   double deltas:          kcat corrections assocated to unique protein
+%                           IDs
 
 % reset random number generator
 rng('default')
+
+% initialize output variables
 pIds = cell(n_repeats,1);
 deltas = cell(n_repeats,1);
 
 for i=1:n_repeats
+    
     rand_idx = randsample(numel(models),set_size,false);
+    
     % run PRESTO with the selected random subset
     [~,~,~,changeTab] = PRESTO(...
         models(rand_idx),...
--- Program/run_robustness_analysis.m
+++ Program/run_robustness_analysis.m
@@ -1,45 +1,72 @@
+% Run this script after running the correct_kcats_yeast.m script, so all
+% required variables are created:
+% models:       condition-specific enzyme-constraint models
+% expVal:       experimentally measured specific growth rates
+% epsilon:      allowed fold change for kcat corrections
+% lambda:       parameter tuning the trade-off between introduced 
+%               corrections and relative errors between experimentally
+%               measured and predicted growth rates 
+% theta:        maximum allowed relative error between measured and
+%               predicted growth rates
 
+% run PRESTO to obtain the set of corrections with all conditions used
 [~,~,~,changeTab] = PRESTO(models,expVal,E,...
     'epsilon',epsilon,'lambda',lambda,'theta',theta);
 
 [pids_all,uniq_idx] = unique(erase(changeTab.(1),'prot_'));
 delta_all = changeTab{uniq_idx,3};
 
+% set the number of iterations and set sizes for robustness analysis
 n_repeats = 10;
 set_sizes = [3 5 10 15];
 
 JI_mats = cell(numel(set_sizes),1);
 corr_P_mats = cell(numel(set_sizes),1);
 corr_S_mats = cell(numel(set_sizes),1);
+
 plot_mats = cell(numel(set_sizes),1);
+
 ji_presto_full = zeros(numel(set_sizes),n_repeats);
 corr_P_presto_full = zeros(numel(set_sizes),n_repeats);
 n_shared_presto_full = zeros(numel(set_sizes),n_repeats);
+
 for s = 1:numel(set_sizes)
+    
+    % run robustness analysis
     [pIds,deltas] = RA_PRESTO(models,expVal,E,epsilon,lambda,theta, n_repeats, set_sizes(s));
+    
     JI = zeros(n_repeats);
     correlation_P = zeros(n_repeats);
     correlation_S = zeros(n_repeats);
     plot_mat = ones(n_repeats);
+    
     for i=1:n_repeats-1
+        
         for j=i+1:n_repeats
             
+            % calculate Jaccard index and correlation
             [prot_intersect,idx_i,idx_j] = intersect(pIds{i},pIds{j});
             JI(i,j) = numel(prot_intersect) / ...
                 numel(union(pIds{i},pIds{j}));
             correlation_P(i,j) = corr(deltas{i}(idx_i),deltas{j}(idx_j));
             correlation_S(i,j) = corr(deltas{i}(idx_i),deltas{j}(idx_j),...
                 'type','Spearman');
+            
+            % combine JI and correlation into a composite matrix, which
+            % contained JI in the upper right triangle and correlation in
+            % the lower left triangle
             plot_mat(i,j) = JI(i,j);
             plot_mat(j,i) = correlation_P(i,j);
             
         end
     end
+    
     JI_mats{s} = JI;
     corr_P_mats{s} = correlation_P;
     corr_S_mats{s} = correlation_S;
     plot_mats{s} = plot_mat;
-  
+    
+    % calculate JI and correlation to corrections using the full set
     for i=1:n_repeats
        [prot_intersect,idx_i,idx_full] = intersect(pIds{i},pids_all);
        ji_presto_full(s,i) = numel(prot_intersect) / ...
@@ -50,23 +77,28 @@ for s = 1:numel(set_sizes)
 
 end
 
+% create figure that compares the iterations for each set size
 figure
 tiledlayout('flow')
 letters = {'a','b','c','d'};
 for i=1:numel(set_sizes)
     nexttile
-    colormap('summer')
+    colormap('bone')
     imagesc(plot_mats{i})
     text(-0.1,1,letters{i},...
         'units','normalized',...
         'fontsize',14,...
         'fontweight','bold')
     title(['Size = ' num2str(set_sizes(i))])
-    xlabel('\rho_S','fontsize',14)
+    xlabel('\rho_P','fontsize',14)
     ylabel('JI','fontsize',14)
 end
 colorbar
+set(gcf,'outerposition',[353.6667  115.6667  722.0000  583.3333])
+exportgraphics(gcf,'Results/similarity_random_subsets_presto.tiff','Resolution',300)
 
+% create figure that compares the results from each iteration and set size
+% with the solution obtained using all experimental conditions
 figure
 colormap('bone')
 tiledlayout('flow')
@@ -98,6 +130,6 @@ hcb = colorbar;
 hcb.Title.String = "\rho_P";
 
 set(gcf,'Outerposition',[353.6667  357.0000  817.3333  342.0000])
-exportgraphics(gcf,'../Results/robustness_presto.tiff','Resolution',300)
+exportgraphics(gcf,'Results/robustness_presto.tiff','Resolution',300)
 
 save('robustness_ws')
