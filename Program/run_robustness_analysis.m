080b296ee43f5e4c61a991c0ad12dc782cbffc98
pwendering <philipp.wendering@gmail.com>
2022-12-07 12:19:08 +0200

    Update number of samples: 50

--- Program/run_robustness_analysis.m
+++ Program/run_robustness_analysis.m
@@ -17,7 +17,7 @@
 delta_all = changeTab{uniq_idx,3};
 
 % set the number of iterations and set sizes for robustness analysis
-n_repeats = 10;
+n_repeats = 50;
 set_sizes = [3 5 10 15];
 
 JI_mats = cell(numel(set_sizes),1);
@@ -95,8 +95,8 @@ for i=1:numel(set_sizes)
 end
 colorbar
 set(gcf,'outerposition',[353.6667  115.6667  722.0000  583.3333])
-exportgraphics(gcf,'Results/similarity_random_subsets_presto.tiff','Resolution',300)
-
+exportgraphics(gcf,'Results/similarity_random_subsets_presto_yeast_n50.tiff','Resolution',300)
+% exportgraphics(gcf,'Results/similarity_random_subsets_presto_ecoli_n50.tiff','Resolution',300)
 % create figure that compares the results from each iteration and set size
 % with the solution obtained using all experimental conditions
 figure
@@ -130,6 +130,50 @@ hcb = colorbar;
 hcb.Title.String = "\rho_P";
 
 set(gcf,'Outerposition',[353.6667  357.0000  817.3333  342.0000])
-exportgraphics(gcf,'Results/robustness_presto.tiff','Resolution',300)
+exportgraphics(gcf,'Results/robustness_presto_yeast_n50.tiff','Resolution',300)
+% exportgraphics(gcf,'Results/robustness_presto_ecoli_n50.tiff','Resolution',300)
 
-save('robustness_ws')
+%% boxplot
+tiledlayout('flow')
+nexttile
+boxchart(...
+    ji_presto_full',...
+    'LineWidth',1.3,...
+    'JitterOutliers','on'...
+);
+ylim([0.9*min(min(ji_presto_full)) 1.05*max(max(ji_presto_full))])
+xticklabels(set_sizes)
+y_ticks = get(gca,'YTick');
+set(gca,'YTick',y_ticks(y_ticks<=1))
+xlabel('Set size')
+ylabel('Jaccard Index')
+text(-0.15,1,letters{1},...
+        'units','normalized',...
+        'fontsize',14,...
+        'fontweight','bold')
+set(gca,'FontSize',14,'FontName','Arial','Box','on','LineWidth',1.3)
+
+    
+nexttile
+boxchart(...
+    corr_P_presto_full',...
+    'LineWidth',1.3,...
+    'JitterOutliers','on'...
+);
+ylim([0.9*min(min(corr_P_presto_full)) 1.05*max(max(corr_P_presto_full))])
+xticklabels(set_sizes)
+y_ticks = get(gca,'YTick');
+set(gca,'YTick',y_ticks(y_ticks<=1))
+xlabel('Set size')
+ylabel('\rho_P')
+text(-0.15,1,letters{2},...
+        'units','normalized',...
+        'fontsize',14,...
+        'fontweight','bold')
+    
+set(gca,'FontSize',14,'FontName','Arial','Box','on','LineWidth',1.3)
+set(gcf,'OuterPosition',1000*[-1.4103    0.1557    0.7320    0.3200])
+
+exportgraphics(gcf,'Results/robustness_presto_yeast_boxplot.tiff','Resolution',300)
+% exportgraphics(gcf,'Results/robustness_presto_ecoli_boxplot.tiff','Resolution',300)
+
