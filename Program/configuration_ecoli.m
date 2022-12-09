93652cb79aef0d3abd1086f83c69ebec4e7820ca
pwendering <philipp.wendering@gmail.com>
2022-12-09 17:29:51 +0200

    Prepend organism-specific GECKO functions to Matlab path

--- Program/configuration_ecoli.m
+++ Program/configuration_ecoli.m
@@ -53,11 +53,17 @@ catch
     error('COBRA toolbox is not installed')
 end
 
-% get maximum kcat value
+% add organism-specific GECKO functions to path
 geckoDir = fullfile(topDir, 'GECKO_E_coli');
+addpath(genpath(fullfile(geckoDir, 'geckomat')), '-begin')
+
+% get maximum kcat value
 maxKcatFile = fullfile(geckoDir, 'databases', 'max_KCAT.txt');
+K = retrieveMaxKcat(maxKcatFile,orgName);
+clear maxKcatFile
+
 % model and data files
 modelFile = fullfile(geckoDir, 'models', 'ecEcoli', 'rawecEcoli.mat');
 batchModelFile = fullfile(geckoDir, 'models', 'ecEcoli', 'rawecEcoli_batch.mat');
-K = retrieveMaxKcat(maxKcatFile,orgName);
-clear maxKcatFile
+
+
